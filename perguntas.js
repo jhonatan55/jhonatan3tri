315b03c813177ca151620c5fc4f837b60a717c71
jhonatan55 <165838888+jhonatan55@users.noreply.github.com>
2024-10-01 14:48:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criacartao(
+    'sociologia',
+    'O que é sociologia',
+    'sociologia e o estudo cientifico da organizacao e do funcionamento das sociedades humanas.'
+)
+
+criaCartao(
+    'empreendedorismo',
+    'o que e empreendedorismo',
+    'e o processo de iniciativa de implementar novos negocios.'
+)
+
+criaCartao(
+    'programacao',
+    'O que é HTML?',
+    'e uma linguagem de marcacao utilizado na construcao de paginas na web.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz ajuda em Inglês?',
+    'Oi em ingles é help '
+)
+criaCartao(
+    'portugues',
+    'o que e genero textual',
+    'sao as classificacoes usadas para determinar os textos com suas caracteristicas'
+)
+
+criaCartao(
+    'educacao financeira ',
+    'o que e capital',
+    'e o conjunto de ativos que movimentam a economia e podem ser conservadas em dinheiro'
+)
+
